447f8c09d4dcf3fdf27706511323e303f764458c
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-02 17:24:48 -0300

    Iniciando projeto Talker Manager

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
