9872293a109170f38d8cb58f94773940579c1b21
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-02 18:32:53 -0300

    Cria endpoint GET /talker que lê um arquivo .json e retorna seus dados. Req 01 concluido

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,10 +8,23 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  let talkers = [];
+   fs.readFile('talker.json', (_err, content) => {    
+     if (!content) return response.status(200).json([]);
+     if (content) {
+      const stringContent = content.toString('utf-8');
+      if (stringContent.length > 0) {
+        talkers = JSON.parse(stringContent);
+      }          
+     } 
+     return response.status(200).json(talkers);
+  });  
 });
 
 app.listen(PORT, () => {
