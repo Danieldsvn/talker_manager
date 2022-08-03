46ebbe9542d076ee9f1c27909f45eaac0f9689f3
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-02 22:03:04 -0300

    Cria userValidation.js com funções de validação de email e password

---
+++ userValidation.js
@@ -0,0 +1,35 @@
+const ValidateEmail = (email) => {
+ if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return (true);
+  }    
+    return (false);
+};
+
+const emailResponse = (response, email) => {
+  if (!email || email === '') {
+    return response.status(400)
+    .json({ message: 'O campo \'email\' é obrigatório' });
+  } 
+  if (!(ValidateEmail(email))) {
+    return response.status(400)
+    .json({ message: 'O \'email\' deve ter o formato \'email@email.com\'' });
+  }
+};
+
+const passwordResponse = (response, password) => {
+  if (!password || password === '') {
+    return response.status(400)
+    .json({ message: 'O campo \'password\' é obrigatório' });
+  } 
+  if (password.length < 6) {
+    return response.status(400)
+    .json({ message: 'O \'password\' deve ter pelo menos 6 caracteres' });
+  }
+};
+
+const userValidation = (request, response, email, password) => {
+  emailResponse(response, email);
+  passwordResponse(response, password);
+};
+
+module.exports = userValidation;
